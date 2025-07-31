0a06f7765204a03dcc327bbad0c6e6f84039eeb7
amir_rafigh <amir.rafigh.work@gmail.com>
2025-07-31 08:57:10 +0330

    complete navbar and add Login Dashboard

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,18 +1,29 @@
 import { RiContactsBook3Fill } from "react-icons/ri";
 import { MdAddCircleOutline } from "react-icons/md";
+import { BiSolidDashboard } from "react-icons/bi";
+import { CgLogIn } from "react-icons/cg";
+
 import Styles from "./navbar.module.css"
 import Link from "next/link";
 import { useRouter } from "next/router";
+
 export default function Navbar(){
     const route = useRouter().pathname    
     return(
         <>
         <div className={Styles.container_navbar}>
+
+            <Link href="/contacts">
+                <h1> <CgLogIn className={route=="/contacts"?Styles.icon_svg:""} />Login </h1>
+            </Link>
             <Link href="/contacts">
                 <h1> <RiContactsBook3Fill className={route=="/contacts"?Styles.icon_svg:""} />Contacts </h1>
             </Link>
             <Link href="/contacts/add">
                 <h1> <MdAddCircleOutline className={route=="/contacts/add"?Styles.icon_svg:""} /> Add Contact</h1>
+            </Link>
+            <Link href="/contacts/add">
+                <h1> <BiSolidDashboard className={route=="/contacts/add"?Styles.icon_svg:""} /> Dashboard</h1>
             </Link>
         </div>
         </>
