c1102891037696a3a89f38fe40c2e5e2f870f417
amir-rafigh <amir.rafigh.work@gmail.com>
2025-08-29 16:22:25 +0330

    fix the logout part for logout clearly

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -1,12 +1,29 @@
 import { serialize } from "cookie";
 
 export default async function handler(req , res){
+    const isProd = process.env.NODE_ENV === "production";
+
     res.setHeader("Set-Cookie" , serialize("token" , "" , {
-        sameSite:"strict",
+        sameSite:"lax",
         path:"/",
-        httpOnly:"true",
-        maxAge:0       
+        httpOnly:true,
+        maxAge:0  ,
+        secure: isProd,
+     
 
     }))
     res.status(200).json({message:"successfully logout"})
 }
+
+
+
+// const isProd = process.env.NODE_ENV === "production";
+
+// const cookie = serialize("token", "", {
+//   httpOnly: true,
+//   secure: isProd,
+//   sameSite: "lax",   // اگر هنگام set هم lax بوده
+//   path: "/",
+//   maxAge: 0,
+//   expires: new Date(0),
+// });
